c8aab00b458b8252c9cfa7085ea48dd9baff3aa5
blackd000 <dongminhcho89@gmail.com>
2022-05-06 23:56:22 +0700

    change

---
+++ learning/encryption/Encrypt.java
@@ -0,0 +1,9 @@
+package learning.encryption;
+
+public class Encrypt {
+
+  public static void main(String[] args) {
+
+    new Frame();
+  }
+}
--- learning/encryption/Frame.java
+++ learning/encryption/Frame.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Frame {
-  
   private char firstKey;
   private Scanner input;
   private ArrayList<Character> list;
   private ArrayList<Character> shuffledList;
 
   public Frame() {
-
     firstKey = ' ';
     input = new Scanner(System.in);
     list = new ArrayList<Character>();
@@ -21,29 +19,31 @@ public class Frame {
   }
 
   private void start() {
-
     while(true) {
       for(int i = 0; i < 30; i++) {
         System.out.println();
       }
 
       System.out.println("(N)ewKey, (G)etKey, (E)ncrypt, (D)ecrypt, (Q)uit");
-      System.out.println("Enter your choice(N,G,E,D,Q): ");
+      System.out.print("Enter your choice(N,G,E,D,Q): ");
 
       char response = Character.toUpperCase(input.nextLine().charAt(0));
 
       switch(response) {
       case 'N':
-
         newKey();
-
+        System.out.println("Bitch");
         break;
 
       case 'G':
-        
         getKey();
-        
         break;
+
+      case 'Q':
+        return;
+
+      default:
+        System.out.println("what the fuck are you doing, please type the key that make sense");
       }
     }
   }
